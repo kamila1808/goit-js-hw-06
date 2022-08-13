f7e9c62d5d556259181e65cd550dd2ebd59fe93d
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2022-08-13 21:08:24 +0600

    Добавляет решение 8-го задания

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+const form = document.querySelector(".login-form");
+
+
+form.addEventListener("submit", onFormSubmit);
+
+
+function onFormSubmit(event) {
+event.preventDefault();
+
+const formElements = event.currentTarget.elements;
+
+console.dir(event.currentTarget.elements);
+
+const email = formElements.email.value;
+const password = formElements.email.password;
+
+if (email === "" || password === "") {
+    return alert("Все поля должны быть заполнены");
+}
+
+const formData = new FormData(event.currentTarget);
+console.log(formData); 
+
+formData.forEach((value, name) => {
+    console.log("onFormSubmit -> name", name );
+    console.log("onFormSubmit -> value", value );
+})
+event.currentTarget.reset(); 
+}
+
+
+
 
