d15606fb1d535c59f74a839b0978cd3c8bccd406
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2022-08-13 10:26:57 +0600

    Добавляет решение 1-го задания

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const ulCategoriesItemEl = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${ulCategoriesItemEl.length}`);
+
+
+ulCategoriesItemEl.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+  });
